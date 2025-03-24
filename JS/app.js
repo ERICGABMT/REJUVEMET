1dc6c4e7ee0aa35b0adcd77db2fc17aafce66240
Eric Martínez <ergamartinezto@ittepic.edu.mx>
2025-03-23 19:57:34 -0700

    Prueba de rejuvemet para el git pages

---
+++ JS/app.js
@@ -0,0 +1,34 @@
+//Aplicaciones bsaicas con JS
+function agregarUsuario(){
+    let nombre = document.getElementById("Nombre").value.trim();
+    let apellidos= document.getElementById("Apellidos").value.trim();
+    let domicilio = document.getElementById("Domicilio").value.trim();
+    let edad= document.getElementById("Domicilio").value.trim();
+    let telefono = document.getElementById("Domicilio").value.trim();
+
+    if (nombre === "" || apellidos === "" || domicilio === "" || edad === "" || telefono === "") {
+        alert("Por favor, complete todos los campos.");
+        return;
+
+    }
+     // Enviar datos al backend
+     fetch("http://localhost:3000/registrar", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ nombre, apellidos, domicilio, edad, telefono, alergias })
+    })
+    .then(response => response.json())
+    .then(data => {
+        alert(`Bienvenido: ${nombre}, tu cuenta se registró correctamente.`);
+        
+        // Redirigir después de 1 segundo
+        setTimeout(() => {
+            window.location.href = "index_iniciar_sesion.html"; 
+        }, 1000);
+    })
+    .catch(error => {
+        console.error("Error:", error);
+        alert("Hubo un problema con el registro, intenta de nuevo.");
+    });
+
+}//END_agregarUsuario
